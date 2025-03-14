2b43bb9284df6f756492955798ba31838460aa34
Jeremy Bosse <bosse.jeremy11@gmail.com>
2025-03-13 21:19:19 -0400

    Error handlers added

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -7,6 +7,10 @@ def sentiment_analyzer(text_to_analyse):  # Define a function named sentiment_an
     header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}  # Set the headers required for the API request
     response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
     formatted_response = json.loads(response.text)
-    label = formatted_response['documentSentiment']['label']
-    score = formatted_response['documentSentiment']['score']
+    if response.status_code == 200:
+        label = formatted_response['documentSentiment']['label']
+        score = formatted_response['documentSentiment']['score']
+    elif response.status_code == 500:
+        label = None
+        score = None
     return {'label': label, 'score': score}  # Return the response text from the API
